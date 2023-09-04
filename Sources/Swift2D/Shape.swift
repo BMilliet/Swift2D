e1bb32398efed745f4061209536e6ea291093024
BMilliet <brunomilliet89@gmail.com>
2023-09-04 11:28:46 -0300

    adjust shape

--- Sources/Swift2D/Shape.swift
+++ Sources/Swift2D/Shape.swift
@@ -1,13 +1,22 @@
 public final class Shape {
     public let id: String
     public var matrix: [[Int]]
-    public var column: Int
-    public var row: Int
+
+    private var _column: Int
+    private var _row: Int
 
     public init(id: String, matrix: [[Int]], column: Int, row: Int) {
         self.id = id
         self.matrix = matrix
-        self.column = column
-        self.row = row
+        self._column = column
+        self._row = row
+    }
+
+    var column: Int { _column }
+    var row: Int { _row }
+
+    func set(column: Int? = nil, row: Int? = nil) {
+        if let column = column { _column = column }
+        if let row = row { _row = row }
     }
 }
